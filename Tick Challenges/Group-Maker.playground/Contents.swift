5b5f6af1daea5fe4f57e22810b5cb41d971f2c55
Russell Gordon <rgordon@rsgc.on.ca>
2016-01-28 10:09:37 -0500

    Initial group setup for Tick.

---
+++ Tick Challenges/Group-Maker.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+//Matthew Anderson
+//Stuart Armstrong
+//Puneet Bagga
+//Noah Barney
+//Scott Blackwell
+//Mattias Blum
+//Jeremy Burleton
+//Liam Byrne
+//Gilbert Chien
+//Thomas Davies
+//Maxym Dubczak
+//Andrew Elder
+//Oliver Gane
+//Jeffrey Goldsmith
+//William Graham
+//Michael Green
+//Nicholas Jones
+//Brendan Leder
+//Alex Lo
+//Hudson McDaniel
+//Robert Miller
+//Carlos Noble Curveira
+//Matthew Pritchard
+//Sean Trevor
+
+var str = "Hello, playground"
