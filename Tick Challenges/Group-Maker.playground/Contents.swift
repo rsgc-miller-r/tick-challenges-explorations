83a87a44409ccb1472ff7fb42b306e70f7f998b4
Russell Gordon <rgordon@rsgc.on.ca>
2016-02-03 16:12:22 -0500

    One possible solution to group randomizing goal
    We use a very basic algorithm to sort a randomized list of numbers from
    highest to smallest.
    
    Input arrays:
    
    - original list of student names
    - list of numbers, randomly generated, one random number tied to each
    student name
    
    Output:
    
    Two new arrays:
    
    - the list of random numbers, now sorted from largest to smallest
    - the list of names, that have been added to a new array in the same
    order as the sorted numbers.
    
    This provides us with an easy to understand, but not very efficient,
    way to shuffle the original list of student names.

--- Tick Challenges/Group-Maker.playground/Contents.swift
+++ Tick Challenges/Group-Maker.playground/Contents.swift
@@ -2,6 +2,8 @@
 
 import Cocoa
 
+// Input Data
+//
 //Matthew Anderson
 //Stuart Armstrong
 //Puneet Bagga
@@ -27,4 +29,146 @@ import Cocoa
 //Matthew Pritchard
 //Sean Trevor
 
-var str = "Hello, playground"
+// Specifications
+//
+// 1. Create 'n' number of groups from the list given.
+// 2. Groups should be randomized.
+
+// Create data structure
+// NOTE: Data structures should be named using camelCasing
+var studentList = [String]()
+studentList.append("Matthew Anderson")
+studentList.append("Stuart Armstrong")
+studentList.append("Puneet Bagga")
+studentList.append("Noah Barney")
+studentList.append("Scott Blackwell")
+studentList.append("Mattias Blum")
+studentList.append("Jeremy Burleton")
+studentList.append("Liam Byrne")
+studentList.append("Gilbert Chien")
+studentList.append("Thomas Davies")
+studentList.append("Maxym Dubczak")
+studentList.append("Andrew Elder")
+studentList.append("Oliver Gane")
+studentList.append("Jeffrey Goldsmith")
+studentList.append("William Graham")
+studentList.append("Michael Green")
+studentList.append("Nicholas Jones")
+studentList.append("Brendan Leder")
+studentList.append("Alex Lo")
+studentList.append("Mark McCutcheon")
+studentList.append("Hudson McDaniel")
+studentList.append("Robert Miller")
+studentList.append("Carlos Noble Curveira")
+studentList.append("Matthew Pritchard")
+studentList.append("Sean Trevor")
+
+// Build a parallel array that has random values for each student
+var studentRandomPosition = [Int]()
+for i in 1...25 {
+    print("\(i)")
+    studentRandomPosition.append(Int(arc4random_uniform(1000)))
+}
+// See what's there...
+studentRandomPosition
+
+// we need to keep track of the position of the number that is the highest so far.  Also, it's index.
+
+var highestValue = -1           // the highest number
+var highestValueIndex = -1      // the highest position
+
+// loop over the list
+for (index, value) in studentRandomPosition.enumerate() {
+    
+    print("The index is \(index) and the value is \(value)")
+    
+    // check the highest value so far against the current value
+    if value > highestValue {
+        
+        // current is higher, so it becomes the new highest
+        highestValue = value
+        highestValueIndex = index
+    }
+    
+}
+
+// Did this work?
+highestValue
+highestValueIndex
+
+
+// *** Now... finish the job.
+//  Very basic algorithm to randomize this list of students
+//
+//      - repeat until the unsorted list is empty
+//          - scan all values in unsorted list, find highest value
+//          - add highest value to the end of the new list
+//          - move the corresponding student name to end of new list 
+//              of names
+//          - remove name and the value from the original lists
+
+// Create the empty sorted list
+var studentListSorted = [String]()  // list of names
+var studentRandomPositionSorted = [Int]()
+
+// Repeat until unsorted list is empty
+while !studentList.isEmpty {    // while NOT studentList is empty
+    
+    // Find the highest value in the current unsorted list
+    highestValue = -1
+    highestValueIndex = -1
+    
+    // loop over the list
+    for (index, value) in studentRandomPosition.enumerate() {
+        
+        print("The index is \(index) and the value is \(value)")
+        
+        // check the highest value so far against the current value
+        if value > highestValue {
+            
+            // current is higher, so it becomes the new highest
+            highestValue = value
+            highestValueIndex = index
+        }
+        
+    }
+    
+    // Put highest value and corresponding name at end of new list
+    studentListSorted.append(studentList[highestValueIndex])
+    studentRandomPositionSorted.append(highestValue)
+    
+    // Remove the highest value and name from the unsorted lists
+    studentList.removeAtIndex(highestValueIndex)
+    studentRandomPosition.removeAtIndex(highestValueIndex)
+    
+}
+
+// What do we have now?
+studentRandomPositionSorted
+studentListSorted
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
